04b67e49c28c0c8d6b2f1ae9cd97216c2e8ef21f
Shadi Ghaith <s_gheith@hotmail.com>
2023-08-07 11:34:02 +0300

    Lesson-8

---
+++ controllers/chatCompletionController.js
@@ -0,0 +1,23 @@
+const openai = require('../config/openaiConfig')
+
+const generateChatCompletion = async (user_role_content) => {
+
+  system_role_content = "You reply as concisely as possible. If you are not sure about an answer, you will respond with \"I don't know\"."
+
+  messages = [
+    {'role': 'system', 'content': system_role_content},
+    {'role': 'user', 'content': user_role_content}
+  ]
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: messages,
+    temperature: 0.5,
+    max_tokens: 512
+  })
+
+  console.log(response.data.choices[0].message.content)
+
+}
+
+module.exports = { generateChatCompletion }
--- playground.js
+++ playground.js
@@ -1,1 +1,13 @@
-console.log("Hello OpenAI API");
+//console.log("Hello OpenAI API");
+
+const readline = require('readline')
+const { generateChatCompletion } = require('./controllers/chatCompletionController');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.question("Enter Prompt to Send for GPT3.5 ChatCompletion Model: \n", 
+            (user_role_content) => generateChatCompletion(user_role_content)
+            )
