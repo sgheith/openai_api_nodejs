37e30ccfc091b0943306523db1b2930949880322
Shadi Ghaith <s_gheith@hotmail.com>
2023-08-10 10:53:18 +0300

    lesson-19

--- playground.js
+++ playground.js
@@ -4,6 +4,7 @@ const readline = require('readline')
 const { generateChatCompletion } = require('./controllers/chatCompletionController');
 const { generateImage, generateImageVariation, generateImageEdit } = require('./controllers/imageController');
 const { generateTranscription, generateTranslation } = require('./controllers/audioController');
+const { generatEembedding, cosineSimilarity } = require('./controllers/embeddingController');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -29,7 +30,44 @@ const rl = readline.createInterface({
 ////////////// AUDIO - START /////////////////////
 
 //generateTranscription("./audio/rr.mp3")
-generateTranslation("./audio/german.mp3")
+//generateTranslation("./audio/german.mp3")
 //generateTranslation("./audio/Fairouz.mp3")
 
 ////////////// AUDIO - END /////////////////////
+
+////////////// EMBEDDINGS - START /////////////////////
+rl.question("Enter Text to Eembed: \n", 
+    (text) => generatEembedding(text).then( 
+          embedding => console.log(embedding)
+       )
+)
+
+// text1 = "Cow"
+// text2 = "Goat"
+text1 = "Capital"
+text2 = "Capitol"
+
+//const text1 = 'Hello, World!';
+//const text2 = 'OpenAI is awesome!';
+//const text3 = 'I feel well today!';
+
+//text1 = 'The boy playing in the street';
+//text2 = 'The kid having fun in the raod';
+//text2 = 'It is a wonderfull morning';
+
+//text1 = "The doctor made an operation"
+//text2 = "The doctor made two operations"
+//text2 = "The surgent made a surgery"
+//text2 = "The engineer fixed the motor"
+//text2 = "Did the engineer fix the motor?"
+
+generatEembedding(text1)
+  .then( embedding1 => {
+    //console.log(embedding)
+    generatEembedding(text2).then( embedding2 => {
+      //console.log(embedding)
+      console.log(cosineSimilarity(embedding1, embedding2))  
+  })
+})
+
+////////////// EMBEDDINGS - END /////////////////////
